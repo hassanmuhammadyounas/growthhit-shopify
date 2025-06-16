31b97dd5b7e186c8b5954e8a6723db811095091f
Hassan Muhammad Younas <hassanmuhammadyounas@gmail.com>
2025-06-16 22:41:54 +0400

    fix: Correct migration for PostgreSQL

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -1,18 +1,19 @@
 -- CreateTable
-CREATE TABLE "Session" (
+CREATE TABLE
+  "Session" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "shop" TEXT NOT NULL,
     "state" TEXT NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "isOnline" BOOLEAN NOT NULL DEFAULT FALSE,
     "scope" TEXT,
-    "expires" DATETIME,
+    "expires" TIMESTAMP(3),
     "accessToken" TEXT NOT NULL,
     "userId" BIGINT,
     "firstName" TEXT,
     "lastName" TEXT,
     "email" TEXT,
-    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT FALSE,
     "locale" TEXT,
-    "collaborator" BOOLEAN DEFAULT false,
-    "emailVerified" BOOLEAN DEFAULT false
-);
+    "collaborator" BOOLEAN DEFAULT FALSE,
+    "emailVerified" BOOLEAN DEFAULT FALSE
+  );
