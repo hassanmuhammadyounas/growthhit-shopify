757795b55864a67564c0c1e3fbf1412edf69c55b
Hassan Muhammad Younas <hassanmuhammadyounas@gmail.com>
2025-06-17 19:45:54 +0400

    fix: consolidate remix config to resolve build error

--- remix.config.cjs
+++ remix.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
     v3_relativeSplatPath: true,
     v3_throwAbortReason: true,
     v3_lazyRouteDiscovery: true,
-    v3_singleFetch: true,
+    v3_singleFetch: false,
     v3_routeConfig: true,
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -53,14 +53,6 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: false,
-        v3_routeConfig: true,
-      },
     }),
     tsconfigPaths(),
   ],
