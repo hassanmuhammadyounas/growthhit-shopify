09cc3f9d11ebd38d8d9d3923eae1c8fb5d42a29c
Hassan Muhammad Younas <hassanmuhammadyounas@gmail.com>
2025-06-19 19:39:14 +0400

    Fixed Deployment - v1

--- server.js
+++
@@ -1,9 +0,0 @@
-import { createRequestHandler } from "@vercel/node";
-
-// Import the server build
-const build = await import("./build/server/index.js");
-
-export default createRequestHandler({
-  build: build.default || build,
-  mode: process.env.NODE_ENV,
-});
